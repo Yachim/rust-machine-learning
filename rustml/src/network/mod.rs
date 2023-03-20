2a91ebaae46bf07ee92f260e5aff90c7126924a2
Yachim <jachym.kohout@gmail.com>
2023-03-20 15:51:15 +0100

    Add tests; fix feedforward

--- rustml/src/network/mod.rs
+++ rustml/src/network/mod.rs
@@ -1,7 +1,9 @@
 pub mod multi_layer_perceptron;
 
 use crate::{
-    functions::{activation::ActivationFunc, input_normalizations::NormalizationFunc},
+    functions::{
+        activation::ActivationFunc, cost::CostFunc, input_normalizations::NormalizationFunc,
+    },
     utils::math::dot_product,
 };
 
@@ -65,8 +67,8 @@ pub trait Writable {
     fn set_activated_layer(&mut self, activated_layer: LayerNeurons, layer_i: usize);
 }
 
+/// or initialize
 pub trait Resetable: Writable + BaseNetwork {
-    /// or initialize
     fn reset_params(&mut self) {
         let shape = self.get_shape();
 
@@ -78,18 +80,16 @@ pub trait Resetable: Writable + BaseNetwork {
         let weights_init_funcs = self.get_activation_funcs();
 
         for (layer_index, &layer_neuron_cnt) in shape.iter().enumerate() {
-            let layer_biases: Vec<f32> = vec![0.0; layer_neuron_cnt];
-            new_biases.push(layer_biases);
-
             let layer: LayerNeurons = vec![0.0; layer_neuron_cnt];
             new_layers.push(layer);
 
             let activated_layer: LayerNeurons = vec![0.0; layer_neuron_cnt];
             new_activated_layers.push(activated_layer);
 
-            if layer_index == 0 {
-                continue;
-            } else {
+            if layer_index != 0 {
+                let layer_biases: Vec<f32> = vec![0.0; layer_neuron_cnt];
+                new_biases.push(layer_biases);
+
                 let mut layer_weights: LayerWeights = vec![];
 
                 let prev_layer_neuron_cnt = shape[layer_index - 1];
@@ -119,51 +119,46 @@ pub trait Resetable: Writable + BaseNetwork {
 
 /// testing/predicting
 pub trait Predictable: BaseNetwork + Writable + Readable {
-    fn activate_layer(&mut self, layer_i: usize) {
-        let layer = self.get_layer(layer_i);
+    fn normalize_input(&mut self) {
+        let layer = self.get_layer(0);
 
-        if layer_i == 0 {
-            let normalization_func = self.get_normalization_func().function;
-            let activated_layer = normalization_func(layer);
-            self.set_activated_layer(activated_layer, layer_i);
-        } else {
-            let activation_func = self.get_activation_func(layer_i - 1).function;
-
-            let mut activated_layer: LayerNeurons = vec![];
-            for &neuron in layer {
-                let activated_neuron = activation_func(neuron);
-                activated_layer.push(activated_neuron);
-            }
-
-            self.set_activated_layer(activated_layer, layer_i);
-        }
+        let normalization_func = self.get_normalization_func().function;
+        let activated_layer = normalization_func(layer);
+        self.set_activated_layer(activated_layer, 0);
     }
 
     fn feedforward_layer(&mut self, layer_i: usize) {
-        self.activate_layer(layer_i);
-
-        if layer_i != 0 {
-            let prev_layer = self.get_activated_layer(layer_i - 1);
-            let layer_weights = self.get_layer_weights(layer_i - 1);
-            let layer_biases = self.get_layer_biases(layer_i - 1);
-
-            let new_layer: LayerNeurons = (0..prev_layer.len())
-                .map(|neuron_i| {
-                    let neuron_weights = &layer_weights[neuron_i];
-                    let neuron_bias = layer_biases[neuron_i];
-
-                    let new_neuron = dot_product(prev_layer, neuron_weights) + neuron_bias;
-                    new_neuron
-                })
-                .collect();
-
-            self.set_layer(new_layer, layer_i);
+        if layer_i == 0 {
+            panic!();
         }
+
+        let prev_layer = self.get_activated_layer(layer_i - 1);
+        let layer_weights = self.get_layer_weights(layer_i - 1);
+        let layer_biases = self.get_layer_biases(layer_i - 1);
+
+        let layer_activation_func = self.get_activation_func(layer_i - 1).function;
+
+        let mut new_layer: LayerNeurons = vec![];
+        let mut new_activated_layer: LayerNeurons = vec![];
+        for neuron_i in 0..layer_weights.len() {
+            let neuron_weights = &layer_weights[neuron_i];
+            let neuron_bias = layer_biases[neuron_i];
+
+            let new_neuron = dot_product(prev_layer, neuron_weights) + neuron_bias;
+            new_layer.push(new_neuron);
+
+            let new_activated_neuron = layer_activation_func(new_neuron);
+            new_activated_layer.push(new_activated_neuron);
+        }
+
+        self.set_layer(new_layer, layer_i);
+        self.set_activated_layer(new_activated_layer, layer_i);
     }
 
     fn feedforward(&mut self) {
         let layer_cnt = self.get_shape().len();
-        for layer_i in 0..layer_cnt {
+        self.normalize_input();
+        for layer_i in 1..layer_cnt {
             self.feedforward_layer(layer_i);
         }
     }
--- rustml/src/network/multi_layer_perceptron.rs
+++ rustml/src/network/multi_layer_perceptron.rs
@@ -1,18 +1,48 @@
 use super::{
-    BaseNetwork, LayerNeurons, LayerWeights, NetworkNeurons, NetworkWeights, Readable, Shape,
-    Writable,
+    BaseNetwork, LayerNeurons, LayerWeights, NetworkNeurons, NetworkWeights, Predictable, Readable,
+    Resetable, Shape, Writable,
 };
-use crate::functions::{activation::ActivationFunc, input_normalizations::NormalizationFunc};
+use crate::functions::{
+    activation::ActivationFunc, cost::CostFunc, input_normalizations::NormalizationFunc,
+};
 
 pub struct MultiLayerPerceptron<'a> {
     shape: Shape,
-    normalization_func: &'a NormalizationFunc<'a>,
     activation_funcs: Vec<&'a ActivationFunc<'a>>,
+    cost_func: &'a CostFunc<'a>,
+    normalization_func: &'a NormalizationFunc<'a>,
 
     weights: NetworkWeights,
     biases: NetworkNeurons,
     layers: NetworkNeurons,
     activated_layers: NetworkNeurons,
+}
+
+impl Resetable for MultiLayerPerceptron<'_> {}
+
+impl<'a> MultiLayerPerceptron<'a> {
+    fn new(
+        shape: Shape,
+        activation_funcs: Vec<&'a ActivationFunc>,
+        cost_func: &'a CostFunc,
+        normalization_func: &'a NormalizationFunc,
+    ) -> Self {
+        assert_eq!(activation_funcs.len(), shape.len() - 1);
+        let mut net = Self {
+            shape,
+            activation_funcs,
+            cost_func,
+            normalization_func,
+
+            weights: vec![],
+            biases: vec![],
+            layers: vec![],
+            activated_layers: vec![],
+        };
+
+        net.reset_params();
+        net
+    }
 }
 
 impl BaseNetwork for MultiLayerPerceptron<'_> {
@@ -85,4 +115,112 @@ impl Writable for MultiLayerPerceptron<'_> {
     fn set_activated_layer(&mut self, activated_layer: LayerNeurons, layer_i: usize) {
         self.activated_layers[layer_i] = activated_layer
     }
+}
+
+impl Predictable for MultiLayerPerceptron<'_> {}
+
+#[cfg(test)]
+mod tests {
+    use super::{MultiLayerPerceptron, Predictable};
+    use crate::functions::{
+        activation::SIGMOID, cost::MSE, input_normalizations::NO_NORMALIZATION,
+    };
+
+    #[test]
+    fn test_ws_cnt() {
+        let net = MultiLayerPerceptron::new(
+            vec![3, 2, 3, 2],
+            vec![&SIGMOID, &SIGMOID, &SIGMOID],
+            &MSE,
+            &NO_NORMALIZATION,
+        );
+
+        let mut total_ws = 0;
+        for i in net.weights {
+            for j in i {
+                for _ in j {
+                    total_ws += 1;
+                }
+            }
+        }
+
+        assert_eq!(total_ws, 18);
+    }
+
+    #[test]
+    fn test_bias_cnt() {
+        let net = MultiLayerPerceptron::new(
+            vec![3, 2, 3, 2],
+            vec![&SIGMOID, &SIGMOID, &SIGMOID],
+            &MSE,
+            &NO_NORMALIZATION,
+        );
+
+        let mut total_biases = 0;
+        for i in net.biases {
+            for _ in i {
+                total_biases += 1;
+            }
+        }
+
+        assert_eq!(total_biases, 7);
+    }
+
+    #[test]
+    fn test_neurons_cnt() {
+        let net = MultiLayerPerceptron::new(
+            vec![3, 2, 3, 2],
+            vec![&SIGMOID, &SIGMOID, &SIGMOID],
+            &MSE,
+            &NO_NORMALIZATION,
+        );
+
+        let mut total_neurons = 0;
+        for i in net.layers {
+            for _ in i {
+                total_neurons += 1;
+            }
+        }
+
+        assert_eq!(total_neurons, 10);
+    }
+
+    #[test]
+    fn test_feedforward_layer() {
+        let mut net =
+            MultiLayerPerceptron::new(vec![2, 3], vec![&SIGMOID], &MSE, &NO_NORMALIZATION);
+
+        println!("{:?}", net.weights);
+        println!("{:?}", net.biases);
+        println!();
+
+        println!("{:?}", net.layers);
+        println!("{:?}", net.activated_layers);
+        println!();
+
+        net.activated_layers[0] = vec![3.0, 2.0];
+        net.weights[0] = vec![vec![3.0, 4.0], vec![2.0, 4.0], vec![3.0, 5.0]];
+        net.biases[0] = vec![1.0, 1.0, 3.0];
+
+        net.feedforward_layer(1);
+
+        println!("{:?}", net.weights);
+        println!("{:?}", net.biases);
+        println!();
+
+        println!("{:?}", net.layers);
+        println!("{:?}", net.activated_layers);
+        println!();
+
+        assert_eq!(net.layers[1], vec![18.0, 15.0, 22.0]);
+    }
+
+    /*#[test]
+    fn test_output() {
+        let net = MultiLayerPerceptron::new(vec![3, 3], vec![&SIGMOID], &MSE, &NO_NORMALIZATION);
+
+        net.activated_layers[0] = vec![3.0, 2.0, 1.0];
+        let out = net.get_output_str();
+        assert_eq!(out, "1");
+    }*/
 }
