e25a682bd88cfc0c090c3fbdf6e6b2156ca5b977
Yachim <jachym.kohout@gmail.com>
2023-03-20 11:08:36 +0100

    Implement BaseNetwork for MultiLayerPerceptron

--- rustml/src/network/multi_layer_perceptron.rs
+++ rustml/src/network/multi_layer_perceptron.rs
@@ -1,1 +1,26 @@
+use super::{BaseNetwork, Shape};
+use crate::functions::{activation::ActivationFunc, input_normalizations::NormalizationFunc};
 
+pub struct MultiLayerPerceptron<'a> {
+    shape: Shape,
+    normalization_func: &'a NormalizationFunc<'a>,
+    activation_funcs: Vec<&'a ActivationFunc<'a>>,
+}
+
+impl BaseNetwork for MultiLayerPerceptron<'_> {
+    fn get_shape(&self) -> &Shape {
+        &self.shape
+    }
+
+    fn get_normalization_func(&self) -> &NormalizationFunc {
+        &self.normalization_func
+    }
+
+    fn get_activation_funcs(&self) -> &Vec<&ActivationFunc> {
+        &self.activation_funcs
+    }
+
+    fn get_activation_func(&self, layer_i: usize) -> &ActivationFunc {
+        self.activation_funcs[layer_i]
+    }
+}
