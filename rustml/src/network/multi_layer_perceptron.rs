822560a4236ecbe8917add8ad31514a40f037721
Yachim <jachym.kohout@gmail.com>
2023-03-20 11:11:55 +0100

    Implement Readable for MultiLayerPerceptron

--- rustml/src/network/multi_layer_perceptron.rs
+++ rustml/src/network/multi_layer_perceptron.rs
@@ -1,10 +1,15 @@
-use super::{BaseNetwork, Shape};
+use super::{BaseNetwork, NetworkNeurons, NetworkWeights, Readable, Shape};
 use crate::functions::{activation::ActivationFunc, input_normalizations::NormalizationFunc};
 
 pub struct MultiLayerPerceptron<'a> {
     shape: Shape,
     normalization_func: &'a NormalizationFunc<'a>,
     activation_funcs: Vec<&'a ActivationFunc<'a>>,
+
+    weights: NetworkWeights,
+    biases: NetworkNeurons,
+    layers: NetworkNeurons,
+    activated_layers: NetworkNeurons,
 }
 
 impl BaseNetwork for MultiLayerPerceptron<'_> {
@@ -22,5 +27,35 @@ impl BaseNetwork for MultiLayerPerceptron<'_> {
 
     fn get_activation_func(&self, layer_i: usize) -> &ActivationFunc {
         self.activation_funcs[layer_i]
+    }
+}
+
+impl Readable for MultiLayerPerceptron<'_> {
+    fn get_weights(&self) -> &NetworkWeights {
+        &self.weights
+    }
+    fn get_layer_weights(&self, layer_i: usize) -> &super::LayerWeights {
+        &self.weights[layer_i]
+    }
+
+    fn get_biases(&self) -> &NetworkNeurons {
+        &self.biases
+    }
+    fn get_layer_biases(&self, layer_i: usize) -> &super::LayerNeurons {
+        &self.biases[layer_i]
+    }
+
+    fn get_layers(&self) -> &NetworkNeurons {
+        &self.layers
+    }
+    fn get_layer(&self, layer_i: usize) -> &super::LayerNeurons {
+        &self.layers[layer_i]
+    }
+
+    fn get_activated_layers(&self) -> &NetworkNeurons {
+        &self.layers
+    }
+    fn get_activated_layer(&self, layer_i: usize) -> &super::LayerNeurons {
+        &self.activated_layers[layer_i]
     }
 }
