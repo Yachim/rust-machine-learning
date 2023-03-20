e4f52fec1b374675216f7db563f069a68973ba0b
Yachim <jachym.kohout@gmail.com>
2023-03-20 10:19:18 +0100

    Rewrite normalization functions from network to vectors

--- rustml/src/functions/input_normalizations.rs
+++ rustml/src/functions/input_normalizations.rs
@@ -1,14 +1,14 @@
 // https://www.baeldung.com/cs/normalizing-inputs-artificial-neural-network
-use crate::network::Network;
-
 pub struct NormalizationFn<'a> {
-    pub function: &'a dyn Fn(&mut Network),
+    pub function: &'a dyn Fn(&Vec<f32>) -> Vec<f32>,
     pub description: &'a str,
     /// latex formula
     pub formula: &'a str,
 }
 
-fn no_func(_: &mut Network) {}
+fn no_func(input: &Vec<f32>) -> Vec<f32> {
+    input.to_vec()
+}
 pub const NO_NORMALIZATION: NormalizationFn = NormalizationFn {
     function: &no_func,
     description: "",
@@ -16,9 +16,7 @@ pub const NO_NORMALIZATION: NormalizationFn = NormalizationFn {
 };
 
 /// mutates the network so that the input is normalized
-fn normalization(network: &mut Network) {
-    let input = network.input.to_owned();
-
+fn normalization(input: &Vec<f32>) -> Vec<f32> {
     let max = input
         .iter()
         .max_by(|a, b| a.partial_cmp(b).unwrap())
@@ -30,9 +28,14 @@ fn normalization(network: &mut Network) {
         .unwrap();
 
     if max - min > f32::EPSILON {
-        for i in 0..network.input.len() {
-            network.input[i] = (network.input[i] - min) / (max - min)
+        let mut new_input: Vec<f32> = vec![];
+        for i in 0..input.len() {
+            new_input.push((input[i] - min) / (max - min));
         }
+
+        new_input
+    } else {
+        input.to_vec()
     }
 }
 pub const NORMALIZATION: NormalizationFn = NormalizationFn {
